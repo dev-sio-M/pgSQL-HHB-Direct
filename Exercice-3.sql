597906e11006895c95b2f27301c7a8be1dbc6d5f
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-12-12 18:23:44 +0100

    Update Exercice-3.sql

--- Exercice-3.sql
+++ Exercice-3.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION creer_id_internet(
-    nom_client_param VARCHAR(50),
-    prenom_client_param VARCHAR(50)
+    nom_client VARCHAR(50),
+    prenom_client VARCHAR(50)
 ) RETURNS TABLE (
     id_internet VARCHAR(50),
     mdp_internet VARCHAR(50)
@@ -8,9 +8,9 @@ CREATE OR REPLACE FUNCTION creer_id_internet(
 DECLARE
     count_clients INTEGER;
 BEGIN
-    SELECT COUNT(*) INTO count_clients FROM Client c WHERE c.nom_client = nom_client_param AND c.prenom_client = prenom_client_param;
+    SELECT COUNT(*) INTO count_clients FROM Client c WHERE c.nom_client = nom_client AND c.prenom_client = prenom_client;
 
-    id_internet := INITCAP(SUBSTRING(prenom_client_param, 1, 1)) || LOWER(nom_client_param) || COALESCE(count_clients + 1, '');
+    id_internet := INITCAP(SUBSTRING(prenom_client, 1, 1)) || LOWER(nom_client) || COALESCE(count_clients + 1, '');
     mdp_internet := id_internet;
 
     RETURN QUERY SELECT id_internet, mdp_internet;
