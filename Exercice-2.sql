0509fdf29a2c4ce3d36b973b80b89de37465e08d
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-12-12 18:24:36 +0100

    Update Exercice-2.sql

--- Exercice-2.sql
+++ Exercice-2.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE FUNCTION nb_operation_compte_mois(
-    mois_arg INT,
-    annee_arg INT,
-    numero_compte_arg INT,
-    type_compte_arg INT
+    mois INT,
+    annee INT,
+    numero_compte INT,
+    type_compte INT
 ) RETURNS INT AS $$
 DECLARE
     nombre_operations INT;
@@ -10,10 +10,10 @@ BEGIN
     SELECT COUNT(*)
     INTO nombre_operations
     FROM Operation
-    WHERE EXTRACT(MONTH FROM dates::DATE) = mois_arg
-    AND EXTRACT(YEAR FROM dates::DATE) = annee_arg
-    AND (num_compte = CONCAT(numero_compte_arg, '_', type_compte_arg)
-         OR num_compte_1 = CONCAT(numero_compte_arg, '_', type_compte_arg));
+    WHERE EXTRACT(MONTH FROM dates::DATE) = mois
+    AND EXTRACT(YEAR FROM dates::DATE) = annee
+    AND (num_compte = CONCAT(numero_compte, '_', type_compte)
+         OR num_compte_1 = CONCAT(numero_compte, '_', type_compte));
 
     RETURN nombre_operations;
 END;
