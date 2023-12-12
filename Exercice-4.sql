cfcbda33b691077c82fac01b81df92d12157d59f
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-12-12 18:22:47 +0100

    Update Exercice-4.sql

--- Exercice-4.sql
+++ Exercice-4.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE FUNCTION creer_date(mois_arg INT, annee_arg INT) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION creer_date(mois INT, annee INT) RETURNS VOID AS $$
 DECLARE
-    max_jours INT := EXTRACT(DAY FROM (DATE_TRUNC('MONTH', MAKE_DATE(annee_arg, mois_arg, 1)) + INTERVAL '1 MONTH - 1 day'));
+    max_jours INT := EXTRACT(DAY FROM (DATE_TRUNC('MONTH', MAKE_DATE(annee, mois, 1)) + INTERVAL '1 MONTH - 1 day'));
 BEGIN
     FOR jour IN 1..max_jours LOOP
         INSERT INTO Dates(dates)
-        VALUES (TO_CHAR(DATE_TRUNC('MONTH', MAKE_DATE(annee_arg, mois_arg, jour)), 'YYYY-MM-DD'));
+        VALUES (TO_CHAR(DATE_TRUNC('MONTH', MAKE_DATE(annee, mois, jour)), 'YYYY-MM-DD'));
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
